1fa764099c3cf24c4b60dc64e21c4f9d387c3e24
Noufal S <noufalnow@gmail.com>
2024-11-21 16:37:24 +0530

    minor fix

---
+++ tenants-service/src/test/java/com/cboard/rental/tenants/TenantsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cboard.rental.tenants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest // This annotation will automatically detect the main application class
+public class TenantsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // This test ensures the Spring ApplicationContext loads successfully.
+    }
+}
